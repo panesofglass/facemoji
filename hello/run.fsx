88e8eba3c8515cd0146e965c8b9d659d4df0512a
Ryan Riley <ryanriley@live.com>
2016-09-07 10:02:08 -0500

    Add additional function

---
+++ hello/run.fsx
@@ -0,0 +1,9 @@
+open System
+open System.IO
+open System.Net
+open System.Net.Http
+
+type Message = { msg : string }
+
+let Run (req:HttpRequestMessage, log:TraceWriter) =
+    req.CreateResponse(HttpStatusCode.OK, { msg = "hello" })
